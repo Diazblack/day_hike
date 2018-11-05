311adf081a1446b5b4014566b333432a8940b16d
Cesar Jolibois <diazcd85@gmail.com>
2018-11-04 17:41:01 -0700

    added factories

---
+++ spec/factories/trips.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :trip do
+    sequence :name        {|n| "Trip #{n}"}
+    sequence :start_date  {|n| " #{2000 + (n * 1)}#{2}#{n} " }
+    sequence :end_date    {|n| " #{2000 + (n * 1)}#{2}#{n+1} "}
+  end
+end 
