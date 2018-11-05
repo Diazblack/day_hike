20917f0fcf7202b28e6a9aca32d4bf446b86d3e0
Cesar Jolibois <diazcd85@gmail.com>
2018-11-04 19:09:36 -0700

    complete story 2

--- spec/features/user_goes_to_index_spec.rb
+++ spec/features/user_goes_to_index_spec.rb
@@ -7,7 +7,7 @@ describe  'User goes to hiking trips index ' do
     @trail_1 = create(:trail, trip: @trip_1)
     @trail_2 = create(:trail, trip: @trip_1)
     @trail_3 = create(:trail, trip: @trip_2)
-    @trail_4 = create(:trail, trip: @trip_2)  
+    @trail_4 = create(:trail, trip: @trip_2)
 
   end
 
@@ -18,5 +18,13 @@ describe  'User goes to hiking trips index ' do
     expect(page).to have_content(@trip_2.name)
   end
 
+  it 'if i click on the trip name go to trip show page ' do
+    visit trips_path
+
+    click_on "#{@trip_1.name}"
+
+    expect(current_path).to eq(trip_path(@trip_1))
+
+  end
 
 end
